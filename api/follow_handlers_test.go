7e1baccb625d114c4dc09ee9def123837bbe6514
Chris <chris@ob1.io>
2019-11-10 18:43:48 -0500

    Add follow api tests

---
+++ api/follow_handlers_test.go
@@ -0,0 +1,168 @@
+package api
+
+import (
+	"context"
+	"errors"
+	"github.com/cpacia/openbazaar3.0/models"
+	peer "github.com/libp2p/go-libp2p-peer"
+	"net/http"
+	"os"
+	"testing"
+)
+
+func TestFollowHandlers(t *testing.T) {
+	runAPITests(t, apiTests{
+		{
+			name:   "Get my followers",
+			path:   "/v1/ob/followers",
+			method: http.MethodGet,
+			setNodeMethods: func(n *mockNode) {
+				n.getMyFollowersFunc = func() (models.Followers, error) {
+					return models.Followers{
+						"12D3KooWLbTBv97L6jvaLkdSRpqhCX3w7PyPDWU7kwJsKJyztAUN",
+						"12D3KooWBfmETW1ZbkdZbKKPpE3jpjyQ5WBXoDF8y9oE8vMQPKLi",
+					}, nil
+				}
+			},
+			statusCode: http.StatusOK,
+			expectedResponse: func() ([]byte, error) {
+				followers := models.Followers{
+					"12D3KooWLbTBv97L6jvaLkdSRpqhCX3w7PyPDWU7kwJsKJyztAUN",
+					"12D3KooWBfmETW1ZbkdZbKKPpE3jpjyQ5WBXoDF8y9oE8vMQPKLi",
+				}
+				return marshalAndSanitizeJSON(&followers)
+			},
+		},
+		{
+			name:   "Get my following",
+			path:   "/v1/ob/following",
+			method: http.MethodGet,
+			setNodeMethods: func(n *mockNode) {
+				n.getMyFollowingFunc = func() (models.Following, error) {
+					return models.Following{
+						"12D3KooWLbTBv97L6jvaLkdSRpqhCX3w7PyPDWU7kwJsKJyztAUN",
+						"12D3KooWBfmETW1ZbkdZbKKPpE3jpjyQ5WBXoDF8y9oE8vMQPKLi",
+					}, nil
+				}
+			},
+			statusCode: http.StatusOK,
+			expectedResponse: func() ([]byte, error) {
+				following := models.Following{
+					"12D3KooWLbTBv97L6jvaLkdSRpqhCX3w7PyPDWU7kwJsKJyztAUN",
+					"12D3KooWBfmETW1ZbkdZbKKPpE3jpjyQ5WBXoDF8y9oE8vMQPKLi",
+				}
+				return marshalAndSanitizeJSON(&following)
+			},
+		},
+		{
+			name:   "Get followers",
+			path:   "/v1/ob/followers/12D3KooWKLmVDz6sdzMyX1yQpEdCHB7dtxyEr91wPFNoCXEs2hkv",
+			method: http.MethodGet,
+			setNodeMethods: func(n *mockNode) {
+				n.getFollowersFunc = func(ctx context.Context, peerID peer.ID, useCache bool) (models.Followers, error) {
+					if peerID.Pretty() == "12D3KooWKLmVDz6sdzMyX1yQpEdCHB7dtxyEr91wPFNoCXEs2hkv" {
+						return models.Followers{
+							"12D3KooWLbTBv97L6jvaLkdSRpqhCX3w7PyPDWU7kwJsKJyztAUN",
+							"12D3KooWBfmETW1ZbkdZbKKPpE3jpjyQ5WBXoDF8y9oE8vMQPKLi",
+						}, nil
+					}
+					return nil, os.ErrNotExist
+				}
+			},
+			statusCode: http.StatusOK,
+			expectedResponse: func() ([]byte, error) {
+				followers := models.Followers{
+					"12D3KooWLbTBv97L6jvaLkdSRpqhCX3w7PyPDWU7kwJsKJyztAUN",
+					"12D3KooWBfmETW1ZbkdZbKKPpE3jpjyQ5WBXoDF8y9oE8vMQPKLi",
+				}
+				return marshalAndSanitizeJSON(&followers)
+			},
+		},
+		{
+			name:   "Get followers not found",
+			path:   "/v1/ob/followers/12D3KooWKLmVDz6sdzMyX1yQpEdCHB7dtxyEr91wPFNoCXEs2hkv",
+			method: http.MethodGet,
+			setNodeMethods: func(n *mockNode) {
+				n.getFollowersFunc = func(ctx context.Context, peerID peer.ID, useCache bool) (models.Followers, error) {
+					return nil, errors.New("not found")
+				}
+			},
+			statusCode: http.StatusNotFound,
+			expectedResponse: func() ([]byte, error) {
+				return []byte("not found\n"), nil
+			},
+		},
+		{
+			name:   "Get following",
+			path:   "/v1/ob/following/12D3KooWKLmVDz6sdzMyX1yQpEdCHB7dtxyEr91wPFNoCXEs2hkv",
+			method: http.MethodGet,
+			setNodeMethods: func(n *mockNode) {
+				n.getFollowingFunc = func(ctx context.Context, peerID peer.ID, useCache bool) (models.Following, error) {
+					if peerID.Pretty() == "12D3KooWKLmVDz6sdzMyX1yQpEdCHB7dtxyEr91wPFNoCXEs2hkv" {
+						return models.Following{
+							"12D3KooWLbTBv97L6jvaLkdSRpqhCX3w7PyPDWU7kwJsKJyztAUN",
+							"12D3KooWBfmETW1ZbkdZbKKPpE3jpjyQ5WBXoDF8y9oE8vMQPKLi",
+						}, nil
+					}
+					return nil, os.ErrNotExist
+				}
+			},
+			statusCode: http.StatusOK,
+			expectedResponse: func() ([]byte, error) {
+				followers := models.Following{
+					"12D3KooWLbTBv97L6jvaLkdSRpqhCX3w7PyPDWU7kwJsKJyztAUN",
+					"12D3KooWBfmETW1ZbkdZbKKPpE3jpjyQ5WBXoDF8y9oE8vMQPKLi",
+				}
+				return marshalAndSanitizeJSON(&followers)
+			},
+		},
+		{
+			name:   "Get following not found",
+			path:   "/v1/ob/following/12D3KooWKLmVDz6sdzMyX1yQpEdCHB7dtxyEr91wPFNoCXEs2hkv",
+			method: http.MethodGet,
+			setNodeMethods: func(n *mockNode) {
+				n.getFollowingFunc = func(ctx context.Context, peerID peer.ID, useCache bool) (models.Following, error) {
+					return nil, errors.New("not found")
+				}
+			},
+			statusCode: http.StatusNotFound,
+			expectedResponse: func() ([]byte, error) {
+				return []byte("not found\n"), nil
+			},
+		},
+		{
+			name:   "Post follow",
+			path:   "/v1/ob/follow/12D3KooWKLmVDz6sdzMyX1yQpEdCHB7dtxyEr91wPFNoCXEs2hkv",
+			method: http.MethodPost,
+			setNodeMethods: func(n *mockNode) {
+				n.followNodeFunc = func(peerID peer.ID, done chan<- struct{}) error {
+					if peerID.Pretty() != "12D3KooWKLmVDz6sdzMyX1yQpEdCHB7dtxyEr91wPFNoCXEs2hkv" {
+						return errors.New("invalid peerID")
+					}
+					return nil
+				}
+			},
+			statusCode: http.StatusOK,
+			expectedResponse: func() ([]byte, error) {
+				return nil, nil
+			},
+		},
+		{
+			name:   "Post unfollow",
+			path:   "/v1/ob/unfollow/12D3KooWKLmVDz6sdzMyX1yQpEdCHB7dtxyEr91wPFNoCXEs2hkv",
+			method: http.MethodPost,
+			setNodeMethods: func(n *mockNode) {
+				n.unfollowNodeFunc = func(peerID peer.ID, done chan<- struct{}) error {
+					if peerID.Pretty() != "12D3KooWKLmVDz6sdzMyX1yQpEdCHB7dtxyEr91wPFNoCXEs2hkv" {
+						return errors.New("invalid peerID")
+					}
+					return nil
+				}
+			},
+			statusCode: http.StatusOK,
+			expectedResponse: func() ([]byte, error) {
+				return nil, nil
+			},
+		},
+	})
+}
