c6d99e4aaf732750ed842aabc0832bb31e08b811
Chris <chris@ob1.io>
2019-11-27 22:59:49 -0500

    Add test for checkForMorePayments

--- orders/transactions_test.go
+++ orders/transactions_test.go
@@ -3,10 +3,14 @@ package orders
 import (
 	"errors"
 	"github.com/cpacia/openbazaar3.0/database"
+	"github.com/cpacia/openbazaar3.0/events"
 	"github.com/cpacia/openbazaar3.0/models"
 	"github.com/cpacia/openbazaar3.0/models/factory"
+	"github.com/cpacia/openbazaar3.0/orders/pb"
+	"github.com/cpacia/openbazaar3.0/wallet"
 	iwallet "github.com/cpacia/wallet-interface"
 	"testing"
+	"time"
 )
 
 func TestOrderProcessor_processWalletTransaction(t *testing.T) {
@@ -225,5 +229,120 @@ func TestOrderProcessor_processWalletTransaction(t *testing.T) {
 			t.Errorf("Test %d validation failed: %s", i, err)
 		}
 		teardown()
+	}
+}
+
+func TestOrderProcessor_checkForMorePayments(t *testing.T) {
+	op, teardown, err := newMockOrderProcessor()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer teardown()
+
+	wn := wallet.NewMockWalletNetwork(1)
+	wn.Start()
+	wn.Wallets()[0].SetEventBus(op.bus)
+
+	op.multiwallet[iwallet.CtTestnetMock] = wn.Wallets()[0]
+
+	orderOpen, _, err := factory.NewOrder()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	sub, err := op.bus.Subscribe(&events.TransactionReceived{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	addr, err := wn.Wallets()[0].NewAddress()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := wn.GenerateToAddress(addr, iwallet.NewAmount(1000000000000000)); err != nil {
+		t.Fatal(err)
+	}
+
+	select {
+	case <-sub.Out():
+	case <-time.After(time.Second * 10):
+		t.Fatal("Timed out waiting on subscription")
+	}
+
+	fundingTxids := make([]iwallet.TransactionID, 0, 5)
+	for i := 0; i < 5; i++ {
+		wtx, err := wn.Wallets()[0].Begin()
+		if err != nil {
+			t.Fatal(err)
+		}
+		fundingTxid, err := wn.Wallets()[0].Spend(wtx, iwallet.NewAddress(orderOpen.Payment.Address, iwallet.CtTestnetMock), iwallet.NewAmount(orderOpen.Payment.Amount), iwallet.FlNormal)
+		if err != nil {
+			t.Fatal(err)
+		}
+		fundingTxids = append(fundingTxids, fundingTxid)
+		if err := wtx.Commit(); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	order := &models.Order{
+		ID:             "abc",
+		Open:           true,
+		PaymentAddress: orderOpen.Payment.Address,
+	}
+	if err := order.PutMessage(orderOpen); err != nil {
+		t.Fatal(err)
+	}
+	if err := order.PutMessage(&pb.PaymentSent{TransactionID: fundingTxids[1].String()}); err != nil {
+		t.Fatal(err)
+	}
+	if err := order.PutMessage(&pb.Refund{RefundInfo: &pb.Refund_TransactionID{TransactionID: fundingTxids[2].String()}}); err != nil {
+		t.Fatal(err)
+	}
+	if err := order.PutMessage(&pb.OrderCancel{TransactionID: fundingTxids[2].String()}); err != nil {
+		t.Fatal(err)
+	}
+	if err := order.PutMessage(&pb.OrderConfirmation{TransactionID: fundingTxids[3].String()}); err != nil {
+		t.Fatal(err)
+	}
+	if err := order.PutMessage(&pb.DisputeClose{TransactionID: fundingTxids[4].String()}); err != nil {
+		t.Fatal(err)
+	}
+
+	err = op.db.Update(func(tx database.Tx) error {
+		return tx.Save(order)
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	op.checkForMorePayments()
+
+	var order2 models.Order
+	err = op.db.View(func(tx database.Tx) error {
+		return tx.Read().Where("id = ?", order.ID).First(&order2).Error
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	txs, err := order2.GetTransactions()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(txs) != 5 {
+		t.Errorf("Expected 5 tx got %d", len(txs))
+	}
+
+	txidMap := make(map[iwallet.TransactionID]bool)
+	for _, tx := range txs {
+		txidMap[tx.ID] = true
+	}
+
+	for _, txid := range fundingTxids {
+		if !txidMap[txid] {
+			t.Errorf("Tx %s not found", txs[0].ID)
+		}
 	}
 }
