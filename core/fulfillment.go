b295548e9bba8aa375a941471fd86c1163475bbd
Chris <chris@ob1.io>
2020-01-13 23:25:46 -0500

    Stub out FulfillOrder method

--- core/fulfillment.go
+++ core/fulfillment.go
@@ -1,1 +1,8 @@
 package core
+
+import "github.com/cpacia/openbazaar3.0/models"
+
+// FulfillOrder sends an order fulfillment to the remote peer and updates the order state.
+func (n *OpenBazaarNode) FulfillOrder(orderID models.OrderID, fulfillments []models.Fulfillment, done chan struct{}) error {
+	return nil
+}
---
+++ models/fulfillment.go
@@ -0,0 +1,26 @@
+package models
+
+// PhysicalDelivery specifies the delivery information for a physical good.
+type PhysicalDelivery struct {
+	Shipper        string `json:"shipper"`
+	TrackingNumber string `json:"trackingNumber"`
+}
+
+// DigitalDelivery specifies the delivery information for a digital good.
+type DigitalDelivery struct {
+	URL      string `json:"url"`
+	Password string `json:"password"`
+}
+
+// CryptocurrencyDelivery specifies the delivery information for a cryptocurrency listing.
+type CryptocurrencyDelivery struct {
+	TransactionID string `json:"transactionID"`
+}
+
+// Fulfillment contains all the data needed to execute an order fulfillment.
+type Fulfillment struct {
+	ItemIndex              int                     `json:"itemIndex"`
+	PhysicalDelivery       *PhysicalDelivery       `json:"physicalDelivery"`
+	DigitalDelivery        *DigitalDelivery        `json:"digitalDelivery"`
+	CryptocurrencyDelivery *CryptocurrencyDelivery `json:"cryptocurrencyDelivery"`
+}
